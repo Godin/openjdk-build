d255d89ec3e64d5ad8abe3a3be73bed1ac2cd51f
Evgeny Mandrikov <mandrikov@gmail.com>
2019-06-04 13:15:34 +0200

    (WIP)

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#git clone https://github.com/Godin/jdk.git
-#pushd jdk
+git clone https://github.com/Godin/jdk.git
+pushd jdk
 
-curl https://builds.shipilev.net/source-snapshots/shenandoah-jdk11.tar.xz | tar xJf -
-pushd shenandoah-jdk11
+#curl https://builds.shipilev.net/source-snapshots/shenandoah-jdk11.tar.xz | tar xJf -
+#pushd shenandoah-jdk11
 
 bash ./configure
 LOG=cmdlines make hotspot
@@ -12,4 +12,5 @@ make
 make images
 
 popd
-tar -czf shenandoah-jdk11-macosx.tar.gz shenandoah-jdk11/build/macosx-x86_64-normal-server-release/images/jdk/
+#tar -czf jdk13-macosx.tar.gz jdk/build/macosx-x86_64-normal-server-release/images/jdk/
+tar -czf jdk13-macosx.tar.gz jdk/build/macosx-x86_64-server-release/images/jdk/
